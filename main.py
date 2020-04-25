cfddb5968fbb5788f476ffcb1ace6062b32c2076
Asher2009 <asher.nam@gmail.com>
2020-04-25 20:51:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+coder_storyteller = 'Asher'
+character_name = 'Asher'
+age = "10"
+victim = "Noah the Noah"
+accused = "Aidenish Aiden"
+crime = 'stloe all of Noahs chunky bois.'
+clues = 'Big teeth all over the sofa'
+is_male = True
+is_female = False
+print ('This is a 10000000% true story')
+print ('Totally didnt make it up')
+print ('This story happened in coding class...')
+print ('It started when Asher found Noahs storage for chunky bois(they are big chunky thick toys aka barbies) and took all of them to his house.')
+print ('(Just saying there was only 100000 chunky bois)')
+print ('Next thing Asher knows he hears Noah calling the police!')
+print ('Asher very silently escapes just in time to Noahs neighbors house.')
+print ('But lucky for Noah there was clues on Ashers favourite things before he escaped.')
+print ('First there was Big boi teeth all over the sofa...')
+print ('Second there was little peices of hair all over the all over his books...')
+print ('And last of all there was small boy hands all over his homework and legos (Just saying I hate homework and tomatoes)')
+print ('For Ashers terrible crime Asher must...')
+print ('1. he must buy extra chunky bois for Noah ')
+print ('2. He must not play video games until he eats all of his cheetos')
+print ('And last he must only read books for 1st graders')
+print ('TTTTTTHHHHHEEEEE EEEEENNNNNDDDDD')
